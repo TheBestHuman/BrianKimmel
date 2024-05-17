cd5832ca9585bf26d088041f1e2611fe1fae21f2
Brian Kimmel <kimmelb@gmail.com>
2024-05-16 23:23:49 -0400

    added some debugging

--- fetch-blog-posts.js
+++ fetch-blog-posts.js
@@ -7,6 +7,12 @@ const apiUrl = `https://pr.ogra.ms/ghost/api/v3/content/posts/?key=${apiKey}&lim
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        fs.writeFileSync('data/blog-posts.json', JSON.stringify(data.posts, null, 2));
+        console.log('Fetched data:', data); // Debugging line
+        if (data.posts) {
+            fs.writeFileSync('data/blog-posts.json', JSON.stringify(data.posts, null, 2));
+            console.log('Data written to data/blog-posts.json');
+        } else {
+            console.error('No posts found in the API response');
+        }
     })
     .catch(error => console.error('Error fetching posts:', error));
